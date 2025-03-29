58517a46742d680f98223c4e0d164b640bdaa40a
Sky <kevin00730@gmail.com>
2025-03-29 13:10:52 +0800

    feat: add base path for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kevin00730.github.io",
+  base: "hyread-one-search",
   integrations: [tailwind(), icon()],
 });
